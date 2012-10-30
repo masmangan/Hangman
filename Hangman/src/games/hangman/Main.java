1dced9a62fa6c275ce39f02ca0c45a13054bc873
mamangan <marco.mangan@gmail.com>
2012-10-30 10:17:34 -0200

    First commit.

---
+++ Hangman/src/games/hangman/Main.java
@@ -0,0 +1,18 @@
+package games.hangman;
+
+/**
+ * http://en.wikipedia.org/wiki/Hangman_%28game%29
+ * 
+ * @author marco.mangan@gmail.com
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hangman!");
+	}
+
+}
